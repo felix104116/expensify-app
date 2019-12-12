7059118996db9ead56afd80c6a61ff693b72cb94
Felix Pelletier <felixbig@hotmail.fr>
2019-12-11 20:06:31 -0500

    Fixed style

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense,removeExpense,editExpense} from './actions/expenses';
 import {setTextFilter,setStartDate,setEndDate} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,7 +6,6 @@ import { SingleDatePicker } from 'react-dates';
 
 //const date = new Date();
 const date = moment();
-console.log(date.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
     constructor(props){
